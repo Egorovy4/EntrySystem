5a5e6b02e42332b2810c253fd43d80b363859c4c
Egorovy4 <mailbyiwanyshyn@gmail.com>
2019-09-14 18:01:14 +0300

    Added login and registration forms, security

--- EntrySystem/src/main/java/entrySystem/Application.java
+++ EntrySystem/src/main/java/entrySystem/Application.java
@@ -2,8 +2,10 @@ package entrySystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"entrySystem"})
 public class Application {
 
 	public static void main(String[] args) {
---
+++ EntrySystem/src/main/java/entrySystem/service/UserService.java
@@ -0,0 +1,26 @@
+package entrySystem.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import entrySystem.dao.UserRepository;
+import entrySystem.domain.User;
+import entrySystem.domain.UserRoles;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private PasswordEncoder bCryptPasswordEncoder;
+
+	public void save(User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		user.setPasswordConfirm(bCryptPasswordEncoder.encode(user.getPasswordConfirm()));
+		user.setRole(UserRoles.USER);
+		userRepository.save(user);
+	}
+}

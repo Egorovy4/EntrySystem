e4bc72014255bf820312c5efff569a7cd6d2aca1
Egorovy4 <mailbyiwanyshyn@gmail.com>
2019-09-26 16:22:01 +0300

    Added logging

--- EntrySystem/src/main/java/entrySystem/service/StatementService.java
+++ EntrySystem/src/main/java/entrySystem/service/StatementService.java
@@ -2,6 +2,8 @@ package entrySystem.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,14 +13,20 @@ import entrySystem.domain.Statement;
 @Service
 public class StatementService {
 
+	private Logger logger = LoggerFactory.getLogger(UserService.class);
+
 	@Autowired
 	private StatementRepository statementRepository;
 
 	public Statement save(Statement statement) {
+		logger.info("Create new statement: " + statement);
+
 		return statementRepository.save(statement);
 	}
 
 	public List<Statement> findAllStatements() {
+		logger.info("Find all statements");
+
 		return statementRepository.findAll();
 	}
 }

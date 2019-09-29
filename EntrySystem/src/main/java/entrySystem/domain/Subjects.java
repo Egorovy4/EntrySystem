34d94d6fc0cb46a8642fdbbbd2bee70106090cfc
Egorovy4 <mailbyiwanyshyn@gmail.com>
2019-09-29 18:56:41 +0300

    Added internalisation using i18n

--- EntrySystem/src/main/java/entrySystem/domain/Subjects.java
+++ EntrySystem/src/main/java/entrySystem/domain/Subjects.java
@@ -1,5 +1,32 @@
 package entrySystem.domain;
 
 public enum Subjects {
-	MATHEMATIC, ENGLISH, DEUTSCH, UKRAINIAN_LANGUAGE, HISTORY, PHYSIC, GEOGRAPHY, FOREIGN_LITERATURE, INFORMATIC, BIOLOGY, CHEMISTRY, SPORTS, CERTIFICATE_MARK;
+	MATHEMATIC("Mathematic"),
+	ENGLISH("English"),
+	DEUTSCH("Deutsch"),
+	UKRAINIAN_LANGUAGE("Ukrainian language"),
+	HISTORY("History"),
+	PHYSIC("Physic"),
+	GEOGRAPHY("Geography"),
+	FOREIGN_LITERATURE("Foreign literature"),
+	INFORMATIC("Informatic"),
+	BIOLOGY("Biology"),
+	CHEMISTRY("Chemistry"),
+	SPORTS("Sports"),
+	CERTIFICATE_MARK("Certificate mark");
+	
+	private String title;
+	
+	Subjects(String title) {
+		this.title = title;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	@Override
+	public String toString() {
+		return title;
+	}
 }
